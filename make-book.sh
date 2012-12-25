635bcbea94e8235db9f527c06e340ee29edcfacd
Jeff Abrahamson <jeff@purple.com>
2012-12-25 20:23:28 +0100

    Abandon paper book formatting.  Format for Galaxy S2 phone.
    Clean up cover info.

--- make-book.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-n=$(grep '%%Pages' recipes.ps|awk '{print $2}');
-pp=$(for ((i=1;$i<=$n;i++)); do echo $i,$i; done)
-pp=$(echo $pp |  tr ' ' ',')
-psselect -p$pp recipes.ps bookpages.ps
